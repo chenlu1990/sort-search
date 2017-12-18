ed9e446e6d522bb1e35a54f41b0fe103e029f339
chenlu <chenlu@baokeyun.com>
2017-12-18 18:10:10 +0800

    初始化

---
+++ src/main/java/com/example/demo/SortSearchApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortSearchApplication.class, args);
+	}
+}
